2154f9aa763451c8e64f9fb9622fd46063b020a4
nellievropa <nellievropa23@gmail.com>
2023-05-24 15:13:23 +0300

    Дз

--- js/massive-filter-fn.js
+++ js/massive-filter-fn.js
@@ -69,3 +69,32 @@ const r2 = filter([1, 2, 3, 4, 5, 6, 7, 8, 12], callback2);
 //     return value <= 4;
 // });
 console.log(r2);
+
+const fruits = [
+    { name: 'apple', quantity: 200, isFresh: true },
+    { name: 'grapes', quantity: 150, isFresh: false },
+    { name: 'pineapple', quantity: 300, isFresh: true },
+    { name: 'bananas', quantity: 100, isFresh: true },
+];
+
+const getFruitsWithQuantity = function (fruits) {
+    return fruits.quantity >= 120;
+}
+// callback3 = getFruitsWithQuantity
+const r3 = filter(fruits, getFruitsWithQuantity);
+console.log(r3);
+
+function calculateTotalPrice(orderedItems) {
+    let totalPrice = 0;
+    // Change code below this line
+
+    // for (let i = 0; i < orderedItems.length; i += 1) {
+    //   totalPrice += orderedItems[i];
+    // }
+    orderedItems.forEach(function (number, index) {
+        totalPrice += orderedItems[i];
+
+        // Change code above this line
+        return totalPrice;
+    })
+};
