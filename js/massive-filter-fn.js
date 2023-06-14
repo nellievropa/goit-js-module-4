f60e0040d0c347d67e7cf6d08849d33899fb3b36
nellievropa <nellievropa23@gmail.com>
2023-06-14 21:05:30 +0300

    5

--- js/massive-filter-fn.js
+++ js/massive-filter-fn.js
@@ -137,3 +137,34 @@ function getCommonElements(firstArray, secondArray) {
 
 getCommonElements([1, 2, 3], [2, 4]);
 getCommonElements([1, 2, 3], [10, 20, 30]);
+
+class Storage {
+    constructor(items = []) {
+        this.items = items;
+    }
+    getItems() {
+        return this.items;
+    };
+    addItem(newItem) {
+        this.items.push(newItem);
+    };
+    removeItem(itemToRemove) {
+       
+  
+        for (let i = 0; i < this.items.length; i += 1) {
+            if (this.items[i] === itemToRemove) {
+                this.items.splice(i, 1);
+            }
+           
+            //     this.items.filter((item) => !this.itemToRemove);
+            // }
+        }
+    }
+}
+// Change code above this line
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
